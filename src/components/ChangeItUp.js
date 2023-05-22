31878a676af983d023db8d17a46de1cb0af66bab
Angela <angelakulola4@gmail.com>
2023-05-22 15:01:58 +0300

    Completed

--- src/components/ChangeItUp.js
+++ src/components/ChangeItUp.js
@@ -14,7 +14,7 @@ function ChangeItUp() {
         placeholder="Enter search term..."
       />
 
-      <select name="filter" onChange={handleChange}>
+      <select name="filter" onChange={() =>handleChange}>
         <option value="all">Select a filter...</option>
         <option value="completed">Completed</option>
         <option value="incomplete">Incomplete</option>
--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ function Login() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={() => handleSubmit}>
       <input type="text" name="username" placeholder="Enter username..." />
       <input type="password" name="password" placeholder="Enter password..." />
       <button>Login</button>
